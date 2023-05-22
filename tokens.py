24d9059937069e5f2291c93126f4fa367b0ac629
Sahil <68115080+SahilJain8@users.noreply.github.com>
2023-05-22 12:53:08 +0530

    dalle images

---
+++ tokens.py
@@ -0,0 +1,3 @@
+API ="https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2-1"
+token = "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"
+model_url = "https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2"
